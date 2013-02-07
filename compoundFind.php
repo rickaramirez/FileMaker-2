211aed335ddf4dd11045153a4db1c690f3731fb2
Ronny Schrader <rodro@filemaker-entwickler.com>
2013-02-07 17:42:36 +0100

    3

--- compoundFind.php
+++ compoundFind.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <!-- declare charset as UTF-8 -->
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta v="content-type" content="text/html; charset=UTF-8">
 </head>
 <body>
 
